15754f7667e4a09ea1dbb0dacb5dd360a0ebf01c
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-07-18 16:36:10 +0300

    feat: create login page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,5 @@
+import { LoginScreen } from '@/screens/login-screen';
+
 export default function Home() {
-  return (
-    <main className='flex min-h-screen items-center justify-center gap-2'>
-      <div>tailwind</div>
-      <div>tailwind</div>
-      <div>tailwind</div>
-      <div>tailwind</div>
-    </main>
-  );
+  return <LoginScreen />;
 }

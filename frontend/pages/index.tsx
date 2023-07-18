15708104b215fb7aa4f012681e2c5d5875418984
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-07-18 16:00:21 +0300

    Squashed commit of the following:
    commit 391ff4e0b4a439142fb0dfa2ac698823d9484be6
    Author: Dispersioned <71058712+Dispersioned@users.noreply.github.com>
    Date:   Tue Jul 18 15:58:32 2023 +0300
    
        feat: create root layout
    
    commit 14d0274a3587bd3ddc3235d7e041465f57b0af3b
    Author: Dispersioned <71058712+Dispersioned@users.noreply.github.com>
    Date:   Tue Jul 18 15:24:15 2023 +0300
    
        cleanup: boilerplate

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <main>ws-chat</main>;
+  return (
+    <main className='flex min-h-screen items-center justify-center gap-2'>
+      <div>tailwind</div>
+      <div>tailwind</div>
+      <div>tailwind</div>
+      <div>tailwind</div>
+    </main>
+  );
 }

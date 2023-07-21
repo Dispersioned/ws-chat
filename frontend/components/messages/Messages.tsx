a3614f6dd3fa71c2e33a85cd7930f2b1c5e044d2
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-07-21 17:06:51 +0300

    feat: fallback on no messages

--- frontend/components/messages/Messages.tsx
+++ frontend/components/messages/Messages.tsx
@@ -9,6 +9,9 @@ export function Messages({ messages }: MessagesProps) {
   return (
     <div className='max-h-[70vh] basis-full overflow-auto rounded border p-2 pr-2 shadow-sm'>
       <div className='flex  flex-col gap-1'>
+        {!messages.length && (
+          <div className='text-gray-500'>No messages yet</div>
+        )}
         {messages.map((message) => (
           <Message key={message.id} message={message} />
         ))}
